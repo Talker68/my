35f83cd03300ff8fb471005d4b71ae0cfc94f5bc
konstantin.skok <konstantin.skok@polyplastic.ru>
2016-03-24 13:44:20 +0300

    project settings

---
+++ app/main/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+import 'angular';
+import 'bootstrap/dist/css/bootstrap.css';
+import uirouter from 'angular-ui-router';
+
+const app = angular.module('app', [uirouter]);
+
+export default app;
