3dc051d8ee820c16cd5556d06ac6bfd055bb73e1
konstantin.skok <konstantin.skok@polyplastic.ru>
2016-04-13 11:06:06 +0300

    confirm order v 0.2

--- app/orders/components/confirmOrder/canActivate.js
+++ app/orders/components/confirmOrder/canActivate.js
@@ -1,17 +1,20 @@
 "use strict";
 
-export default function($nextInstruction, $prevInstruction, VehicleService , DriversService, $q) {
+export default function($nextInstruction, $prevInstruction, VehicleService , DriversService, $q, $timeout) {
 
   return $q.all(
     {
       drivers : DriversService.getDriversList(),
-      vehicle : VehicleService.getVehicleList()
+      vehicle : VehicleService.getVehicleList(),
+      loadingTypes : VehicleService.getLoadingTypes()
     }
   ).then(
     (response) => {
       $nextInstruction.routeData.drivers = response.drivers.data;
       $nextInstruction.routeData.vehicle = response.vehicle.data;
+      $nextInstruction.routeData.loadingTypes = response.loadingTypes;
     }
   )
+
 
 }
--- app/orders/components/confirmOrder/controller.js
+++ app/orders/components/confirmOrder/controller.js
@@ -2,26 +2,35 @@
 
 export default function ($rootRouter, DriversService) {
 
-
   //получаем guid заказа из параметра и сам заказ
   this.$routerOnActivate = function(next, previous) {
     this.orderGuid = next.params.guid;
-    this.drivers = next.routeData.drivers;
-    this.vehicle = next.routeData.vehicle;
+    this.driversList = next.routeData.drivers;
+    this.vehicleList = next.routeData.vehicle;
+    this.loadingTypes = next.routeData.loadingTypes;
   };
 
   this.driverFromList = true;
   this.vehicleFromList = true;
 
 
-  //установка транспортной компании для заявки
-  this.setFrowarder = function(forwarderGuid, orderAcceptTime){
-    return OrdersService.patchOrder(orderGuid, {forwarderGuid : forwarderGuid, orderAcceptTime : orderAcceptTime}).then(
-      (response) => {
-        $rootRouter.navigate(['Orders', 'OrdersList']);
-      }
-    )
-  };
+  this.test = function () {
+    alert('fefe');
+  }
+
+  this.submit = function(){
+    console.log(this);
+  }
+
+  //
+  // //установка транспортной компании для заявки
+  // this.setFrowarder = function(forwarderGuid, orderAcceptTime){
+  //   return OrdersService.patchOrder(orderGuid, {forwarderGuid : forwarderGuid, orderAcceptTime : orderAcceptTime}).then(
+  //     (response) => {
+  //       $rootRouter.navigate(['Orders', 'OrdersList']);
+  //     }
+  //   )
+  // };
 
 
 
