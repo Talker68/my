e41c29a4e4a50dd6e5dc63ae5b84d8efd9c89458
y.shushunov <yuriy.shushunov@polyplastic.ru>
2016-04-19 17:39:38 +0300

    новый список

--- app/vehicle/components/semitrailerDetail/controller.js
+++ app/vehicle/components/semitrailerDetail/controller.js
@@ -1,18 +1,18 @@
 "use strict";
  
-export default function (VehicleService) {
+export default function (VehicleService, ApiService) {
   this.isEdit = false;
 
   this.edit = function(){
     this.isEdit = true;
-    this.copyToEdit = angular.copy(this.semitrailer);
-  }
+    this.copyToEdit = angular.copy( this.semitrailer );
+  };
 
   this.cancelEdit = function(){
     this.isEdit = false;
-  }
+  };
 
-  //редактирование полуприцепа
+  // редактирование полуприцепа
   this.updateSemitrailer = function(semitrailer){
     return VehicleService.updateSemitrailer(semitrailer).then(
       (response) => {
@@ -20,9 +20,19 @@ export default function (VehicleService) {
         this.isEdit = false;
       }
     )
-  }
+  };
+
+  // удаление полуприцепа
+  this.removeSemitrailer = function () {
+      return VehicleService.removeSemitrailer( this.semitrailer ).then(
+          (response) => {
+              debugger;
+              let index = ApiService.getIndexById(this.semitrailersList, {fieldName : 'guid', value : this.semitrailer.guid });
+              this.semitrailersList.splice(index,1);
+          }
+      )
+  };
   
   this.remove = function () {
-    
   }
 }
