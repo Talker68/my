b1bc0e427aee9f85aa97cec7dd9e21e70a0351dd
yuriy.shushunov <yuriy.shushunov@polyplastic.ru>
2016-11-21 16:54:57 +0300

    Требования к паролю

--- app/forwarders/components/company/controller.js
+++ app/forwarders/components/company/controller.js
@@ -1,5 +1,24 @@
 "use strict";
 
-export default function($rootScope){
+export default function($rootScope, $sce){
   this.company = $rootScope.currentUser;
+
+    this.htmlPopover = $sce.trustAsHtml(
+        '<div class="s-title">Требования к паролю:</div>' +
+        '<ul style="margin-right: 20px">' +
+        '<li>Не меньше 8 латинских букв и цифр</li>'+
+        '<li>Как минимум одна большая буква</li>'+
+        '<li>Как минимум одна маленькая буква</li>'+
+        '<li>Нельзя использовать 5 последних паролей</li>'+
+        '</ul>'+
+        '<div class="s-title">Для смены пароля:</div>'+
+        '<ul style="margin-right: 20px">'+
+        '<li>Укажите "Имя пользователя" с префиксом ppl/имя пользователя</li>'+
+        '<li>Правильно укажите текущий пароль</li>'+
+        '<li>Внимательно введите новый</li>'+
+        '<li>Повторите ввод нового пароля.</li>'+
+        '</ul>'
+    );
+
+
 }
