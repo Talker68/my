7c6337581b7500952d1ff405b3582f1efd91e216
Konstantin Skok <konstantin.skok@polyplastic.ru>
2016-11-22 11:33:58 +0300

    при отправке заявки напрямую убран доп. запрос

--- app/orders/components/order/directOrder/controller.js
+++ app/orders/components/order/directOrder/controller.js
@@ -12,12 +12,7 @@ export default function(ForwardersService, OrdersService){
     };
 
     OrdersService.directOrder(this.resolve.orderGuid, requestData).then(
-      response => {
-        // TODO после изменения запроса, отправлять ответ запроса вместо получения заявки отельным запросом
-        OrdersService.getOrderByGuid(this.resolve.orderGuid).then(
-          response => {this.close({$value: response.data[0]})}
-        )
-      }
+      response => this.close({$value: response.data})
     )
   }
 
