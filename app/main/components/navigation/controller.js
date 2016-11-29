151e0d40b5c51be591b69f312c5d90d574dd3c03
Konstantin Skok <konstantin.skok@polyplastic.ru>
2016-11-29 16:10:48 +0300

    fix error  rejector

--- app/main/components/navigation/controller.js
+++ app/main/components/navigation/controller.js
@@ -4,5 +4,6 @@ export default function(OrdersService) {
   this.$onInit = function(){
     this.ORDER_STATUSES = OrdersService.ORDER_STATUSES;
     this.AUCTION_STATUSES = OrdersService.AUCTION_STATUSES;
+    this.LIST_ORDER_TYPES = OrdersService.LIST_ORDER_TYPES;
   }
 }
--- app/main/services/errorRejector.js
+++ app/main/services/errorRejector.js
@@ -2,16 +2,15 @@
 
 export default function($injector, $q) {
   this.responseError = (rejection) => {
-    // if(rejection.status >= 500){
-    //   $injector.get('ModalService').showError("Ошибка сервера");
-    // } else {
+    if(rejection.status >= 500){
+      $injector.get('ModalService').showError("Ошибка сервера");
+    } else {
       if(rejection.data[0] === '<'){
         $injector.get('ModalService').showError(rejection.statusText);
       } else {
         $injector.get('ModalService').showError(rejection.data);
       }
-
-    //}
+    }
     
     return $q.reject(rejection)
   }
